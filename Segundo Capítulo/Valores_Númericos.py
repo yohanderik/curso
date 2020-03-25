2e605871e0fafd893ddcf92c0a46271612e8021b
Yohan Derik <yohan.derik.silva@gmail.com>
2020-03-25 14:44:01 -0300

    Finalização do Valores Númericos

---
+++ Segundo Capítulo/Valores_Númericos.py
@@ -0,0 +1,14 @@
+#Inteiros(int): 5, 78, -467 ou 23786874236487263786423
+#Números de ponto flutuante(float): 1.0, 45.2222, -46.78
+#Booleanos(bool): “True” e “False”
+#Números complexos(complex): (3 + 4j), (-2 + 4.5j), (56.2 - 7.67j)
+
+#x = 10, x(int)
+#x = 4 + 9j, x(complex)
+
+x = 10                 #atribuimos um número inteiro a X
+print(type(x))
+y = 3.45               #atribuimos um número de ponto flutuante a Y
+print(type(y))
+z = 3j                 #atribuimos um número complexo a Z
+print(type(z))
