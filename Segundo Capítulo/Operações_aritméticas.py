7c10bab6cc3b0396d2ef6d802c6903fa12348816
Yohan Derik <yohan.derik.silva@gmail.com>
2020-03-26 15:25:29 -0300

    Iniciação das operações aritméticas

---
+++ Segundo Capítulo/Operações_aritméticas.py
@@ -0,0 +1,24 @@
+a = 3
+b = 4
+
+print(a + b)
+print(3 + 4)
+
+print(a - b)
+print(3 - 4)
+
+print(a * b)
+print(3 * 4)
+
+print(a / b)
+print(3 / 4)
+
+print(a // b)
+print(3 // 4)
+
+print(a % b)
+print(3 % 4)
+
+print(a ** b)
+print(3 ** 4)
+
