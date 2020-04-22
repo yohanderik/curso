fef7f8cf7719f9af324601d2158b58e61ad62f07
Yohan Derik <yohan.derik.silva@gmail.com>
2020-04-22 17:46:12 -0300

    Exercícios 2.2

---
+++ Segundo Capítulo/EXERCÍCIOS_2.py
@@ -0,0 +1,19 @@
+1. #Observe o programa a seguir e calcule, com o auxílio de lápis e papel, quais resultados serão impressos. Depois execute o programa e verifique se você acertou. Depois leia a resolução deste exercício disponível online onde introduzimos um importante conceito para análise de programas: a tabela de rastreamento.
+
+x = 45
+y = 14
+z = x // y
+
+print(z)
+
+z = y % z
+
+print(x)
+
+y = 10
+
+print(z)
+
+#R_ X = 45
+   #Y = 14
+   #Z = 3
