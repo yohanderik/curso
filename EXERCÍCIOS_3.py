506c57c984a1dd7da5b6d2b6459edb873f4f9eba
Yohan Derik <yohan.derik.silva@gmail.com>
2020-04-27 16:43:03 -0300

    EXERCÍCIOS 3

---
+++ EXERCÍCIOS_3.py
@@ -0,0 +1,13 @@
+1. #Transforme os comandos a seguir para a notação que usa a mensagem “format”. Lembre-se que argumentos separados por vírgula adicionam um espaço entre eles na impressão:
+
+    • print(x, 'e', y, 'são', a, 'e', b)
+    • print('Total:', x+y)
+    • print(a, b, c, 'são números inteiros') 
+    • print(z, '+ 0 =', z)
+
+
+#R_ 
+     print(.format(x, 'e', y, 'são', a, 'e', b))
+     print(.format('Total:', x + y))
+     print(.format(a, b, c, 'são números inteiros'))
+     print(.format(z, '+ 0 =', z))
