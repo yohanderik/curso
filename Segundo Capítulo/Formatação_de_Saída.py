2fc8b2a2a486091b81f1e4e767c57007852deaa5
Yohan Derik <yohan.derik.silva@gmail.com>
2020-04-27 16:17:58 -0300

    ARRUMANDO

--- Segundo Capítulo/Formatação_de_Saída.py
+++ Segundo Capítulo/Formatação_de_Saída.py
@@ -4,7 +4,8 @@ y = 87
 print(x, '//', y, '=', x // y)
 print(x, '%', y, '=', x % y)                              #Estilo menos recomentadovel
 
-print('{0} // {1} = {2}' .format(x, y, x // y))           #Estilo mais recomentavel
+print('{0} // {1} = {2}' .format(x, y, x // y))           
+print('{0} % {1} = {2}' .format(x, y, x % y))             #Estilo mais recomentavel
 
 #x.mensagem(y)                Errado
 #mensagem(x,y)                Certo
