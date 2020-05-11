cdac0b0c0eccea05ee1d073adddcd78427159f5d
Yohan Derik <yohan.derik.silva@gmail.com>
2020-05-11 16:57:44 -0300

    Padronizando e Corrigindo Nomes

--- Segundo Capítulo/EXERCÍCIOS_4.py
+++ Segundo Capítulo/EXERCÍCIOS_4.py
@@ -1,13 +1,13 @@
 #1_ Considere que as variáveis “dia”, “mês” e “ano” contém os valores respectivos de uma certa data. Escreva um comando “print” usando a mensagem “format” que imprima essa data no formato usual, por exemplo, “12/11/2016” ou “3/7/2011”
 
-Dia = 12
-Mês = 11
-Ano = 2016
+dia = 12
+mes = 11
+ano = 2016
 
 print('{0}/{1}/{2}'.format(12, 11, 2016))
 
-Dia = 3
-Mês = 7
-Ano = 2011
+dia = 3
+mes = 7
+ano = 2011
 
 print('{0}/{1}/{2}'.format(3, 7, 2011))
