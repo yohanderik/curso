5ef75da8d1d4308646a228c143273133985e82fd
Yohan Derik <yohan.derik.silva@gmail.com>
2020-05-13 18:19:06 -0300

    Entrada de dados

---
+++ Segundo Capítulo/Entrada _de_Dados_Pelo_Teclado.py
@@ -0,0 +1,62 @@
+#x = input('Qual o valor de x?  ')
+#y = input('Qual o valor de y?  ')
+
+#print('Divisão=', x // y)
+#print('Resto=', x % y)
+
+# = TypeError: unsupported operand type(s) for //: 'str' and 'str'
+
+#x = input('Entre com um número:')
+#print(type(x))
+
+# = <class 'str'>
+
+#x = '8'
+#print(int(x))
+
+# = 8
+
+#x = int('8')
+#print(type(x))
+
+# = <class 'int'>
+
+x = int(input('Qual o valor de x?  '))
+y = int(input('Qual o valor de y?  '))
+
+print('Divisão =', x // y)
+print('Resto =', x % y)
+
+#x = int(input('Qual o valor de x? 5'))
+#y = int(input('Qual o valor de y? 9'))
+
+#print('Divisão =', 5 // 9)
+#print('Resto =', 5 % 9)
+
+#Divisão = 0
+#Resto = 5
+
+#x = int(input('Qual o valor de x? Cinco'))
+#y = int(input('Qual o valor de y? Nove'))
+
+#print('Divisão =', Cinco // Nove)
+#print('Resto =', Cinco % Nove)
+
+# = ValueError: invalid literal for int() with base 10: ‘abc’
+
+x = float(input('Qual o valor de x?  '))
+y = float(input('Qual o valor de y?  '))
+
+print('Divisão =', x // y)
+print('Resto =', x % y)
+
+#Para números com de ponto flutuante
+
+#x = float(input('Qual o valor de x? 5.7'))
+#y = float(input('Qual o valor de y? 9.2'))
+
+#print('Divisão =', 5.7 // 9.2)
+#print('Resto =', 5.7 % 9.2)
+
+#Divisão = 0.0
+#Resto = 5.7
