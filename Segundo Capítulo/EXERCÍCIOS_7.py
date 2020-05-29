c619bb3fca9c50f61bbb37d4ac5a12652d923c22
Yohan Derik <yohan.derik.silva@gmail.com>
2020-05-28 22:35:49 -0300

    EXERCICIOS 7, 8

---
+++ Segundo Capítulo/EXERCÍCIOS_7.py
@@ -0,0 +1,9 @@
+#1_ Escreva um programa que leia um número e escreva o valor da raiz cúbica deste:
+
+#R_
+
+print('Escreva o valor de X e eu darei a raiz cubica dele:')
+
+X = int(input('Qual o valor de X?  '))
+
+print('O resultado e:', X ** (1 / 3))
---
+++ Segundo Capítulo/EXERCÍCIOS_8.py
@@ -0,0 +1,11 @@
+#1_ Escreva um programa que leia o numero de uma INDICE e o numero do RADICANDO e escreva o valor de indice √ radicando = RAIZ
+
+#R_
+
+print('Escreva o valor do INDICE e o valor do RADICANDO e eu farei o RADICAL entre esses numeros com o resultado da RAIZ entre eles')
+
+INDICE = int(input('Qual o valor do INDICE?   '))
+
+RADICANDO = int(input('Qual o valor do RADICANDO?   '))
+
+print('O valor da RAIZ entre esses numeros e:', RADICANDO ** (1 / INDICE))
