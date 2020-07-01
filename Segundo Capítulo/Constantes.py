2f759f8c40f7905af5d19233235484baefeb1cba
Yohan Derik <yohan.derik.silva@gmail.com>
2020-07-01 15:57:32 -0300

    Constantes

---
+++ Segundo Capítulo/Constantes.py
@@ -0,0 +1,19 @@
+R = float(input('Entre com um raio:   '))
+
+print('Circuferencia:', 2 * 3.14159 * R)
+print('Área do circulo:', 3.14159 * R ** 2)                                      #Forma errada
+print('Área da esfera:', 4 * 3.14159 * R ** 2)
+print('Volume da esfera:', 4 / 3 * 3.14159 * R ** 3)
+
+
+
+PI = 3.14159
+
+R = float(input('Entre com um raio:   '))
+
+print('Circuferencia:', 2 * PI * R)
+print('Área do circulo:', PI * R ** 2)                                           #Forma certa
+print('Área da esfera:', 4 * PI * R ** 2)
+print('Volume da esfera:', 4 / 3 * PI * R ** 3)
+
+PI = 0/1
