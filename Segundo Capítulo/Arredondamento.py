80db05b5cb555a84a4bfc3fb70eeaa06fa8f52a6
Yohan Derik <yohan.derik.silva@gmail.com>
2020-07-06 20:11:12 -0300

    Arrendondamento

---
+++ Segundo Capítulo/Arredondamento.py
@@ -0,0 +1,30 @@
+#n = round('Um numero')             Arrendonda o numero para o numero inteiro mais proximo
+
+#Ex:
+N = round(2.7)      # = 3
+print(N)
+
+N = round(5.1)      # = 4
+print(N)
+
+
+#n = int('Um numero')               Remove a virgula do numero
+
+#Ex:
+N = int(2.7)      # = 2
+print(N)
+
+N = int(5.1)      # = 5
+print(N)
+
+
+
+N = round(334.123453, 2)
+print(N)
+
+
+
+P = 10.25
+D = round(P * 0.9, 2)
+
+print(D)
