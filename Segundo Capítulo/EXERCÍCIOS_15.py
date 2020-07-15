d9fa7eb8f1c3b8427801ff501b8db113a46f2a1e
Yohan Derik <yohan.derik.silva@gmail.com>
2020-07-15 18:15:40 -0300

    EXERCÍCIOS_15

---
+++ Segundo Capítulo/EXERCÍCIOS_15.py
@@ -0,0 +1,13 @@
+#1_ Represente em notação científica os seguintes números:
+
+#• 0.000000513532181
+
+#R_
+
+print(round(0.000000513532181 * (10 ** 15)))
+
+#• 59012210122
+
+#R_
+
+print(59012210122 * (10 ** -12))
