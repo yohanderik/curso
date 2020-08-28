751f48c5562e829fafc639a2649b6b61eda5930a
Yohan Derik <yohan.derik.silva@gmail.com>
2020-08-28 19:31:39 -0300

    Contagem Simples - Variável de Interação

---
+++ Terceiro Capítulo/Contagem_Simples-Variável_de_Iteração.py
@@ -0,0 +1,17 @@
+#for Variável_de_Interação in Campo_de_Interação:
+   #print(Variável_de_Interação, 'Comando_a_ser_Repetido')
+
+#for Variável_de_Interação in range(Quantidade_de_Repetições):
+   #print(Variável_de_Interação, 'Comando_a_ser_Repetido')
+
+#ex:
+
+lista_de_letras = ['a', 'b', 'c', 'd']
+
+for i in range(4):
+    for letra in lista_de_letras:
+        print("letra")
+
+
+for i in range(100):
+    print('Eu não fiz isso!')
