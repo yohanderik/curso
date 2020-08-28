a948b81802e36cb6932ff92dbcc63436cda84406
Yohan Derik <yohan.derik.silva@gmail.com>
2020-08-28 19:32:03 -0300

    Contagem a Partir de Valores Quaisquer

---
+++ Terceiro Capítulo/Contagem_a_Partir_de_Valores_Quaisquer.py
@@ -0,0 +1,10 @@
+#for i in range(valor inicial, valor final)
+    #print(i, 'Eu não fiz isso')
+
+#ex:
+
+for i in range(-30, 45):
+    print(i, 'Eu não fiz isso')
+
+for i in range(45, -30):
+    print(i, 'Eu não fiz isso')
