a8e4253fd04daee2befa44f5145c9bf96225b9f3
Yohan Derik <yohan.derik.silva@gmail.com>
2021-01-11 20:20:42 -0500

    Vários Comandos Subordinados a uma Estrutura

---
+++ Terceiro Capítulo/Vários_Comandos_Subordinados_a_uma_Estrutura.py
@@ -0,0 +1,9 @@
+#Comandos de Repetição
+
+#for i in range(5):
+#    print('Eu não fiz isso!')
+#    print('Brincadeirinha...')           Este comando esta sendo subordinado ao for
+
+for i in range(5):
+    print('Eu não fiz isso!')
+print('Brincadeirinha...')               #Desta forma o comando não esta mais sendo subordinado ao for
